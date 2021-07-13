9e0d2db36c70762791f28312581122b5e38de143
Nathan Thomas Valentim Sircus <xinovebig@gmail.com>
2021-07-13 14:54:26 -0300

    Update main.ino

--- main/main.ino
+++ main/main.ino
@@ -49,9 +49,9 @@ int send_data(void)
       // make a HTTP request:
       // send HTTP header
       client.println("POST " + PATH_NAME + " HTTP/1.1");
-      //client.println("Host: " + String(HOST_NAME));
+      client.println("Host: " + String(HOST_NAME));
       client.println("Content-Type: application/json");
-      //client.println("Content-Length: " + data_send.length());
+      client.println("Content-Length: " + String(data_send.length()));
       client.println("Connection: close");
       client.println(); // end HTTP header
 
