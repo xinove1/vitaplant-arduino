e98056bb0e181d96a852aa23af21335938dbee69
Nathan Thomas Valentim Sircus <xinovebig@gmail.com>
2021-07-20 19:27:39 -0300

    SEXO?

--- main/interrogacao.C
+++ main/interrogacao.C
@@ -3,16 +3,17 @@
 #include <ArduinoJson.h>
 #include <string.h>
 
+
+
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 int SensorHL = A0;
 int rele = 7;
 
-EthernetClient client;
-
-struct data {
-  int bomb;
-  int led;
+struct Data {
+    int bomba;
+    int led;
 };
+EthernetClient client;
 
 int    HTTP_PORT   = 80;
 String HTTP_METHOD = "POST"; // or "POST"
@@ -74,9 +75,14 @@ int send_data(void)
         }
       }
       
-      Serial.println("sexo2.0:" + parse_http(c) + "a");
+      Serial.println("sexo2.0:" + parse_http(c));
+      if (parse_http(c) == "201")
+      {
+        Serial.println("201");
+        return(1);
+      }
     }
-    
+  Serial.println("200");
   return(0);
 }
 
@@ -87,26 +93,40 @@ void receive_data(void)
       // if connected:
       Serial.println("Connected to server");
 
+      String data_send;
+      fill_data_send(&data_send);
       // make a HTTP request:
       // send HTTP header
       client.println("GET " + PATH_NAME + " HTTP/1.1");
       client.println("Host: " + String(HOST_NAME));
+      client.println("Content-Type: application/json");
+      client.println("Content-Length: " + String(data_send.length()));
       client.println("Connection: close");
       client.println(); // end HTTP header
 
-      //Serial.println(data_send);   
+      //Serial.println(data_send);
+      client.println(data_send);      
       String c ;
     
       while(client.connected()) {
-        if(client.available()){
+        int i = 0;
+        if(client.available() && i < 10){
           // read an incoming byte from the server and print it to serial monitor:
           char ca = client.read();
           c += ca;
+          i++;
         }
       }
       
-      Serial.println("sexo2.0:" + parse_http(c) + "B");
+      Serial.println("sexo5.0:" + parse_http(c));
     }
+}
+
+void bomba(int mili)
+{
+  digitalWrite(rele, HIGH);
+  delay(mili);
+  digitalWrite(rele, LOW);
 }
 
 void fill_data_send(String *data_send)
@@ -134,7 +154,7 @@ String    parse_http(String c)
     int beg = 0;
     int i = 0;
     int j = 0;
-    String dest[4];
+    char *dest[4];
     while (c[i] != '\n')
     {
         if (c[i] == ' ')
@@ -148,10 +168,10 @@ String    parse_http(String c)
         }
         i++;
     }
-    return(dest[1]);
+    return(String(dest[1]));
 }
 
-String    fill(char *c)
+char    *fill(char *c)
 {
     //char cc[20];
     int i = 0;
